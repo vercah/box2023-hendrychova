63e89b00be25f841d62da3d235625cb6bed827c5
Krafpy <nazar.misyats@gmail.com>
2023-12-13 12:46:24 +0100

    Added fasta file loading

--- project/maw/maw.py
+++ project/maw/maw.py
@@ -1,5 +1,34 @@
+from xopen import xopen
+from functools import lru_cache
+from readfa import readfq
+
+NUCLEOTIDE_COMPLEMENT = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
+}
+
+def reverse_complement(seq: str):
+    rev = []
+    for char in reversed(seq):
+        rev.append(NUCLEOTIDE_COMPLEMENT[char])
+    return "".join(rev)
+
+@lru_cache
+def read_fa_sequences(file_name: str):
+    sequences = dict()
+    for seq_name, seq, _ in readfq(xopen(file_name)):
+        if seq_name in sequences:
+            raise NameError("Duplicate sequence name.")
+        sequences[seq_name] = seq
+    return sequences
+
+
 def main():
-    pass
+    sequences = read_fa_sequences("../all_ebi_plasmids.fa.xz")
+    for name, seq in sequences.items():
+        print(name, len(seq))
 
 if __name__ == "__main__":
     main()
