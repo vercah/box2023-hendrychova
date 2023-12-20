47c2ba151018e1790d73afe62b635614f2eb6d63
Krafpy <nazar.misyats@gmail.com>
2023-12-20 17:23:10 +0100

    Added to TSV conversion

--- project/maw/fmt.py
+++ project/maw/fmt.py
@@ -1,3 +1,12 @@
 def pprint(maws: dict[int, set[str]]):
     for k, xs in maws.items():
         print(f"k = {k}: {' '.join(sorted(xs))}")
+
+def to_tsv(maws: dict[int, set[str]], filename: str):
+    lines = []
+    for k, xs in maws.items():
+        if len(xs) == 0:
+            continue
+        lines.append(f"{k}\t{','.join(sorted(xs))}")
+    with open(filename, "w") as f:
+        f.write("\n".join(lines))
