7a097908c295caae6e1f03b65bf69b713f3fe746
Veronika Hendrychova <vercaposta@seznam.cz>
2023-12-21 22:27:18 +0100

    add naive test

--- project/tests/test_naive.py
+++ project/tests/test_naive.py
@@ -1,5 +1,6 @@
 import pytest
 from maw.naive import is_maw
+from maw.naive import find_maws
 
 def test_is_maw():
     assert is_maw("GGA", {"ACCG"}) == False
@@ -8,3 +9,7 @@ def test_is_maw():
     assert is_maw("ACG", {"ACCG"}) == True
     assert is_maw("AAA", {"AACTACT"}) == True
     assert is_maw("TAA", {"AACTACT"}) == True
+
+def find_maws():
+    assert find_maws({"AACTACT"}, 3) == {3: {"AAA", "TAA", "AAG"}}
+    assert find_maws({"AAA"}, 3) == dict()
